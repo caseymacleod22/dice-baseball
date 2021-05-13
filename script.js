4e128efd2afe61127858933e5d9423c6998290ff
Casey MacLeod <caseymacleod22@gmail.com>
2021-05-13 13:03:32 -0400

    adds single pbp

--- script.js
+++ script.js
@@ -33,8 +33,10 @@ function rollDice() {
         let hitResult = dice.roll()
         if (hitResult === 1) {
             hitResult = 'Single'
+            getSinglePBP()
         } else if (hitResult === 2) {
             hitResult = 'Single'
+            getSinglePBP()
         } else if (hitResult ===  3) {
             hitResult = 'Double'
         } else if (hitResult === 4) {
@@ -81,6 +83,29 @@ function rollDice() {
         }
 
     getHitResult()
+
+
+        function getSinglePBP() {
+            const PBP = {
+                
+                "single": [
+                    "Line drive into left field, that'll be a base hit,",
+                    "Line drive into right, he'll reach with a single.",
+                    "A bullet back up the middle for a single!",
+                    "A little flare over first base... that's gonna fall in for a single!",
+                    "Dribbler down the third base line, it will stat fair for an infield single.",
+                    "And that ball is hammered down the right field line! Played beautifully by the right fielder to   hold him to a single",
+                    "Ground ball up the middle, diving stop by the shortstop, but the runner beats it out for an infield single",
+                    "Weak groundball towards third but the shift is on and no one is there! That's gonna be a single.",
+                    "Broken bat pop up and that will drop over the second base bag for a single."
+                ]
+            }
+            let singlePBP = PBP.single
+            let randomNum = Math.floor(Math.random() * singlePBP.length)
+            singlePBP = PBP.single[randomNum]
+            call = singlePBP
+            console.log(singlePBP)
+        }
 }
 
 
