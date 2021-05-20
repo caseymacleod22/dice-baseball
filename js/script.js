0ee72a2cf97c75c772ee83041c5587d971c26d08
Casey MacLeod <caseymacleod22@gmail.com>
2021-05-20 12:10:07 -0400

    wip: increments outs and inning

--- js/script.js
+++ js/script.js
@@ -261,10 +261,10 @@ function rollDice() {
 
         $('#pbp').html(call).fadeIn(500)
         $('#playresult').html(result)
-        // $('#outsnum').html(outs)
-        // $('#inningnum').html(inning)
-        // console.log(outs)
-        // console.log(totalOuts)
+        $('#outsnum').html(outs)
+        $('#inningnum').html(inning)
+        console.log(outs)
+        console.log(totalOuts)
 }
 
 // rollDice()
